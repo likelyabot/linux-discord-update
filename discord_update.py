90f1c6ad8e0834c19357420992edc3ca8c1f8683
likelyabot <67652446+likelyabot@users.noreply.github.com>
2024-05-01 12:41:10 -0700

    Add files via upload

---
+++ discord_update.py
@@ -0,0 +1,57 @@
+import requests
+import subprocess
+import shutil
+import os
+
+url = 'https://discord.com/api/download?platform=linux&format=tar.gz'
+discord_archive = '/dev/shm/discord.tar.gz'
+discord_directory = '/opt/Discord/'
+
+def download_discord(url, discord_archive):
+    try:
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(discord_archive, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print("Successfully downloaded Discord.")
+        else:
+            print(f"Failed to Download. Status code: {response.status_code} - {response.reason}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during download: {str(e)}")
+
+def clean_directory(discord_directory):
+    try:
+        for filename in os.listdir(discord_directory):
+            file_path = os.path.join(discord_directory, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        print("Successfully cleaned directory")
+    except Exception as e:
+        print(f"An error occurred while cleaning the directory: {str(e)}")
+
+def extract_discord(discord_archive, discord_directory):
+    try:
+        result = subprocess.run(['tar', '-xzf', discord_archive, '-C', discord_directory], check=True)
+        if result.returncode == 0:
+            print("Successfully extracted files")
+        else:
+            print(f"Failed to extract files: {result.returncode}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred during extraction: {str(e)}")
+    except Exception as e:
+        print(f"Error during extraction: {str(e)}")
+
+def clean_temp(discord_archive):
+    try:
+        os.remove(discord_archive)
+        print("Successfully removed temporary files.")
+    except Exception as e:
+        print(f"Failed to remove temporary files: {str(e)}")
+
+download_discord(url, discord_archive)
+clean_directory(discord_directory)
+extract_discord(discord_archive, discord_directory)
+clean_temp(discord_archive)
