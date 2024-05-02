e8486b92831bf774c835478967e55a0087375b58
likelyabot <67652446+likelyabot@users.noreply.github.com>
2024-05-01 18:38:23 -0700

    Update discord_update.py

--- discord_update.py
+++ discord_update.py
@@ -6,6 +6,19 @@ import os
 url = 'https://discord.com/api/download?platform=linux&format=tar.gz'
 discord_archive = '/dev/shm/discord.tar.gz'
 discord_directory = '/opt/Discord/'
+launcher_file = '/usr/share/applications/discord.desktop'
+desktop_contents = """
+[Desktop Entry]
+Name=Discord
+StartupWMClass=discord
+Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
+GenericName=Internet Messenger
+Exec=/opt/Discord/Discord/Discord
+Icon=/opt/Discord/Discord/discord.png
+Type=Application
+Categories=Network;InstantMessaging;
+Path=/opt/Discord/Discord/
+"""
 
 def download_discord(url, discord_archive):
     try:
@@ -19,6 +32,15 @@ def download_discord(url, discord_archive):
             print(f"Failed to Download. Status code: {response.status_code} - {response.reason}")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred during download: {str(e)}")
+
+def create_directory():
+    try:
+        isExist = os.path.exists(discord_directory)
+        if not isExist:
+            os.makedirs(discord_directory)
+            print("Successfully created install directory")
+    except Exception as e:
+        print(f"Failed to create directory: {str(e)}")
 
 def clean_directory(discord_directory):
     try:
@@ -50,8 +72,21 @@ def clean_temp(discord_archive):
         print("Successfully removed temporary files.")
     except Exception as e:
         print(f"Failed to remove temporary files: {str(e)}")
+        
+def check_launcher():
+    if not os.path.exists(launcher_file):
+        try:
+            with open(launcher_file, 'w') as file:
+                file.write(desktop_contents.strip())
+            print("Desktop entry created successfully.")
+        except Exception as e:
+            print(f"Failed to create desktop entry: {str(e)}")
+    else:
+        print("Desktop entry already exists.")  
 
 download_discord(url, discord_archive)
+create_directory()
 clean_directory(discord_directory)
 extract_discord(discord_archive, discord_directory)
 clean_temp(discord_archive)
+check_launcher()
